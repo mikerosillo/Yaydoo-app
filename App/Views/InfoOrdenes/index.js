7a71c5d08c56d7c512ec535e3b66d9c240e52209
Miguel Rosillo <miguel.rosillo@yaydoo.com>
2019-12-15 01:21:14 -0600

    details view added

--- App/Views/InfoOrdenes/index.js
+++ App/Views/InfoOrdenes/index.js
@@ -21,18 +21,23 @@ import Moment from 'moment';
 var numeral = require('numeral');
 
 export default class Profile extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
-            data:[],  
+            data:[],
+            folio:'',
+            informacion: true,
+            productos: false,
         }
     };
     
     previewsPage(){
       Actions.profile()
     };
-    componentWillMount(){
+    UNSAFE_componentWillMount(){
         this.state.data = this.props.data
+        this.state.folio = this.props.folio
+        console.log('from will', this.state.data)
     }
     // getAddress(data, tipo){
     //   console.log(tipo)
@@ -85,15 +90,119 @@ export default class Profile extends Component {
     //   }
     //   return count
     // };
-   
+    informacionTitleStyle(){
+        if(this.state.informacion == true){
+            return {
+                textDecorationLine: 'underline',
+                color:'#FFF', 
+                marginLeft:20, 
+                fontSize:18
+            }
+        } else {
+            return {
+                color:'#FFF', 
+                marginLeft:20, 
+                fontSize:18
+            }
+        }
+    };
+    productosTitleStyle(){
+        if(this.state.productos == true){
+            return {
+                textDecorationLine: 'underline',
+                color:'#FFF', 
+                marginLeft:20, 
+                fontSize:18
+            }
+        } else {
+            return {
+                color:'#FFF', 
+                marginLeft:20, 
+                fontSize:18
+            }
+        }
+    };
+    whichToRender(){
+        let arr = [this.state.data]
+        let name = arr.map((el , key)=>{
+            return el.account.user.first_name
+        })
+        
+        if(this.state.productos == true){
+            return <View style={{flexDirection:'row'}}>
+                        <View style={{width:'50%'}}>
+                            <Text style={{color:'#000000'}}>Productos</Text>
+                            <Text style={{color:'#000000'}}>Solicitante</Text>
+                            <Text style={{color:'#000000'}}>Prioridad</Text>
+                            <Text style={{color:'#000000'}}>Entrega</Text>
+                            <Text style={{color:'#000000'}}>Direccion</Text>
+                            <Text style={{color:'#000000'}}>Fecha requerida</Text>
+                        </View>
+                        <View style={{width:'50%'}}></View>
+                    </View>
+        } else {      
+           return   <View style={{flexDirection:'row'}}>
+                        <View style={{width:'50%'}}>
+                            <Text style={{color:'#000000', marginLeft:20, fontSize:15, fontWeight:'bold', marginTop:20, marginBottom:20}}>Detalle</Text>
+                            <Text style={{color:'#000000', marginLeft:20, marginTop:20, marginBottom:20}}>Solicitante</Text>
+                            <Text style={{color:'#000000', marginLeft:20, marginTop:20, marginBottom:20}}>Prioridad</Text>
+                            <Text style={{color:'#000000', marginLeft:20, marginTop:20, marginBottom:20, fontWeight:'bold'}}>Entrega</Text>
+                            <Text style={{color:'#000000', marginLeft:20, marginTop:20, marginBottom:20}}>Direccion</Text>
+                            <Text style={{color:'#000000', marginLeft:20, marginTop:20, marginBottom:20}}>Fecha requerida</Text>
+                        </View>
+                        <View style={{width:'50%'}}>
+                            <Text style={{color:'#000000', fontSize:15, fontWeight:'bold', marginTop:20, marginBottom:20}}></Text>
+                            <Text style={{color:'#000000', marginTop:20, marginBottom:20}}>{name}</Text>
+                            <Text style={{color:'#000000', marginTop:20, marginBottom:20}}>Prioridad</Text>
+                            <Text style={{color:'#000000', marginTop:20, marginBottom:20, fontWeight:'bold'}}></Text>
+                            <Text style={{color:'#000000', marginTop:20, marginBottom:20}}>Direccion</Text>
+                            <Text style={{color:'#000000', marginTop:20, marginBottom:20}}>Fecha requerida</Text>
+                        </View>   
+                    </View>
+                   
+        }
+    }
    
     render() {
-        var data = this.state.data
-        console.log('from',data)
         return (
             <View style={styles.container}>
+                <View style={{backgroundColor:'#4180fd'}}>
+                    <View style={{flexDirection:'row', marginTop:20}}>
+                        <View style={{width:'50%', flexDirection:'row'}}>
+                           <View style={{width:'40%', justifyContent:'center'}}>
+                                <TouchableOpacity  onPress={() => this.previewsPage()}>
+                                    <Image
+                                        source={require('../../../assets/icons8-chevron-left-48.png')}
+                                        // source={{uri : 'assets/icons8-chevron-left-48.png'}}
+                                        style={{ width: 15, height: 15, marginLeft: 20,color:'#FFF'}}
+                                    />
+                                </TouchableOpacity>
+                           </View>
+                           <View style={{width:'60%'}}>
+                               <Text style={{color:'#FFF', fontSize:20}}># {this.state.folio}</Text>
+                           </View>
+                        </View>
+                        <View style={{width:'50%'}}></View>
+                    </View>
+                    <View style={{ flexDirection:'row', marginTop:20, marginBottom:20}}>
+                        <View style={{width:'50%'}}>
+                            <TouchableOpacity  onPress={() => this.setState({informacion: true, productos: false})}>
+                                <Text style={this.informacionTitleStyle()}>INFORMACION</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={{width:'50%'}}>
+                        
+                  
+
+                            <TouchableOpacity onPress={() => this.setState({productos: true, informacion: false})}> 
+                                <Text style={this.productosTitleStyle()}>PRODUCTOS</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
                 <ScrollView style={{marginTop:0}}>
-                    <View style={{marginTop:40}}></View>
+                    {this.whichToRender()}
+    
                 </ScrollView>
             </View> 
         )
@@ -102,7 +211,7 @@ export default class Profile extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "rgb(211,211,211)",
+        backgroundColor: "#FFF",
 
     },
 })
